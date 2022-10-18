bf19b6a237a059b447ccc3c84de13b0ec126ab63
omkar022 <omkarjadhav0199@gmail.com>
2022-10-18 10:51:59 +0530

    Setup

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const PORT = 8081;
+app.use(express.json());
+app.get('/', (req, res) => {
+    res.status(200).json({
+        message: "Server is up and running",
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running at port ${PORT}`);
+});
+
+app.get("*", (req, res) => {
+    res.status(404).json({
+        message: 'This route does not exist'
+    });
+});
